e4bf5175076e0ff0d38e72044c815f8a281b95f9
AndreaZanasi <zanasi.ndr@gmail.com>
2024-04-10 22:07:52 +0200

    Fixed exit statement

--- Risorse/output.asm
+++ Risorse/output.asm
@@ -1,5 +1,16 @@
 global _start
 _start:
+
+     mov rax, 7
+     push rax
+
+     ;;exit
+     mov rax, 60
+     pop rdi
+
+     syscall
+     ;;/exit
+
      mov rax, 60
      mov rdi, 0
      syscall
--- src/main/java/org/compiler/Generator.java
+++ src/main/java/org/compiler/Generator.java
@@ -14,18 +14,24 @@ import org.compiler.token.tokens.TokenIdent;
  */
 public class Generator {
     private final NodeProgram m_program;
+    long stack_location;
 
     public Generator(NodeProgram program) {
         this.m_program = program;
+        this.stack_location = 0;
     }
 
     public String generateStatement(NodeStatement stmt) {
         StringBuilder stmtSB = new StringBuilder();
         switch (stmt) {
             case NodeExit nodeExit -> {
+                //prendo l'espressione exit(expr)
+                stmtSB.append(generateExpression(stmt.getStmt()));
+                stmtSB.append("     ;;exit\n");
                 stmtSB.append("     mov rax, 60\n");
-                stmtSB.append("     mov rdi, ").append("0").append("\n");
-                stmtSB.append("     syscall");
+                stmtSB.append(pop("rdi")).append("\n");
+                stmtSB.append("     syscall\n");
+                stmtSB.append("     ;;/exit\n\n");
             }
             case NodeLet nodeLet -> {
                 // Handle NodeLet type
@@ -39,13 +45,14 @@ public class Generator {
 
     public String generateExpression(NodeExpression expr) {
         StringBuilder exprSB = new StringBuilder();
+
         switch (expr) {
             case NodeIntLit nodeIntLit -> {
                 exprSB.append("     mov rax, ").append(nodeIntLit.getIntLit().getValue()).append("\n");
-                exprSB.append("     push rax\n");
+                exprSB.append(push("rax")).append("\n");
             }
             case NodeIdent nodeIdent -> {
-                // Handle NodeIdent type
+
             }
             case null, default -> {
                 throw new IllegalArgumentException("Unknown expression type in generator");
@@ -56,14 +63,41 @@ public class Generator {
 
     public String generateProgram() {
         StringBuilder sb = new StringBuilder();
-        sb.append("global _start\n_start:\n");
+        sb.append("global _start\n_start:\n\n");
         for (NodeStatement statement : m_program.getStmts()) {
             sb.append(generateStatement(statement));
         }
         // Exits 0 by default
         sb.append("     mov rax, 60\n");
         sb.append("     mov rdi, 0\n");
-        sb.append("     syscall");
+        sb.append("     syscall\n");
         return sb.toString();
+    }
+
+    public void printStmt() {
+        for (NodeStatement statement : m_program.getStmts()) {
+            System.out.println(statement.getStmt().getExpr().getType().toString());
+        }
+        stack_location++;
+    }
+
+    /**
+     * increase stack location
+     * @param reg asm register
+     * @return a string
+     */
+    public String push(String reg) {
+        stack_location++;
+        return "     push " + reg + "\n";
+    }
+
+    /**
+     * reduces stack location
+     * @param reg asm register
+     * @return a string
+     */
+    public String pop(String reg) {
+        stack_location--;
+        return "     pop " + reg + "\n";
     }
 }
