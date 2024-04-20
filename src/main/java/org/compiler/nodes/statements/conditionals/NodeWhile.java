ea74d052001408053a322a5b4be830bb28ada354
AndreaZanasi <zanasi.ndr@gmail.com>
2024-04-21 01:15:18 +0200

    Added while cicle

--- src/main/java/org/compiler/nodes/statements/conditionals/NodeWhile.java
+++ src/main/java/org/compiler/nodes/statements/conditionals/NodeWhile.java
@@ -3,7 +3,7 @@ package org.compiler.nodes.statements.conditionals;
 import org.compiler.nodes.NodeExpression;
 import org.compiler.nodes.statements.NodeScope;
 
-public class NodeWhile extends Conditional{
+public class NodeWhile extends Conditional {
     public NodeWhile(NodeExpression stmt, NodeScope node) {
         super(stmt, node);
     }
